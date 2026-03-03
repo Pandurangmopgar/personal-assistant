96601c8c0590d0a839128908eba3025ca985e8cb
Pandurangmopgar <pandurangmopgar7410@gmail.com>
2026-03-03 17:40:13 +0530

    feat: add initial React application setup in `main.tsx`.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './components/WhatsAppClone.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/WhatsAppClone';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
